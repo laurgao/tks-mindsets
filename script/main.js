b63404b496f4bc8b5bbc36a62ec1483e33df7d4f
Laura Gao <lauragao86868@gmail.com>
2021-12-25 17:19:17 -0500

    Clean up

--- script/main.js
+++ script/main.js
@@ -1,14 +1,11 @@
-// const MINDSETS = [{name: "Seek understanding", description: ""}, "Bias towards action", "Compounding"]
-// const MINDSETS = require('./data.json');
 let MINDSETS;
 fetch("./data.json")
     .then(response => {
         return response.json();
     })
     .then(jsondata => MINDSETS = jsondata);
-console.log(MINDSETS)
+
 async function main() {
-    console.log("js works!")
     await document.fonts.ready;
     const nameTextBox = document.getElementById("title")
     const descriptionTextBox = document.getElementById("description")
